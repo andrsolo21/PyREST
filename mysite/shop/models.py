aa9923bec5d6daaa28a4604d5e52ada745a8ce96
Unknown <andrsolo21@gmail.com>
2019-08-01 20:34:40 +0300

    migrate!!!

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -2,11 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Person(models.Model):
 
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField()
+    class Meta:
+        unique_together = (('import_id', 'citizen_id'),)
+
+    import_id = models.ForeignKey('Imp', on_delete='CASCADE')
+    citizen_id = models.IntegerField()
+    town = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    building = models.CharField(max_length=50)
+    appartement = models.IntegerField()
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=10)
+    #relatives = models.TextField()
+
+class Relatives(models.Model):
+    class Meta:
+        unique_together = (('import_id', 'citizen_id', 'relative_id'),)
+
+    import_id =  models.ForeignKey('Imp', on_delete='CASCADE')
+    citizen_id = models.IntegerField()
+    relative_id = models.IntegerField()
+
+
+class Imp(models.Model):
+    import_id = models.AutoField(primary_key=True)
+    num = models.IntegerField()
